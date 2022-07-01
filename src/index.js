a6f57f45647973dfb5dff6c381d0f8855461c399
Ahmad Ibrahim <ahmadaburabee3@gmail.com>
2022-06-30 18:43:24 -0600

    cleanUp: removed all unnecessary files from create-react-app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+// redux
+import store from "./redux/store"; // import my store.js file
+import { Provider } from "react-redux"; // import the provider from redux
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* wrapping our entire app in the redux provider and passing store as a prop */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
